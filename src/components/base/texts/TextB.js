f1ce3f232eefd1028d4f3cf37fdfc55e1a544a30
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-07-17 15:07:07 +0300

    propTypes added for the props and delete fragment in jsx

--- src/components/base/texts/TextB.js
+++ src/components/base/texts/TextB.js
@@ -1,11 +1,15 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
 const TextB = ({ children, color = 'purple' }) => {
   return (
-    <>
       <p className={'text-b ' + color}>{children}</p>
-    </>
   );
 };
 
 export default TextB;
+
+TextA.propTypes = {
+  children: PropTypes.object.isRequired,
+  color: PropTypes.string.isRequired
+};
