a60ab23f1765193d720dfdd2aaff75bdfdc4cbe4
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-07-17 15:04:08 +0300

    propTypes added for the props

--- src/components/base/texts/TextA.js
+++ src/components/base/texts/TextA.js
@@ -1,5 +1,12 @@
+import PropTypes from 'prop-types';
+
 const TextA = ({ children, color = 'purple' }) => {
   return <p className={'text-a ' + color}>{children}</p>;
 };
 
 export default TextA;
+
+TextA.propTypes = {
+  children: PropTypes.object.isRequired,
+  color: PropTypes.string.isRequired
+};
