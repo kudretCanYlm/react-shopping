4cc883bc123d4221f08f4d2a0a53d9ffacc4fdc1
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-07-18 14:25:04 +0300

    added textChange function

--- src/components/main/navbars/NavBarTop.js
+++ src/components/main/navbars/NavBarTop.js
@@ -3,15 +3,23 @@ import IconButton from '../../base/buttons/IconButton';
 import { AiOutlineMenu } from 'react-icons/ai';
 import SearchTextBox from '../../base/textboxes/SearchTextBox';
 import TextLink from '../../base/links/TextLink';
+import { useState } from 'react';
 
 //menuclick reduxla sağlanacak
 const NavBarTop = ({ MenuClick = (f) => f }) => {
+  const [text, setText] = useState();
+
+  const textChange = (txt) => {
+    setText(txt);
+    console.log(txt + ' ' + text);
+  };
+
   return (
     <div className="nav-bar-top flex-row">
       <IconButton text={'Main'} onClick={MenuClick}>
         <AiOutlineMenu className={'white fontsize-dashboard'} />
       </IconButton>
-      <SearchTextBox placeHolder="Search Here" className="max-width-100" />
+      <SearchTextBox textChange={textChange} placeHolder="Search Here" className="max-width-100" />
       <TextLink to={'/dashboard'} className={'white'}>
         Dashboard
       </TextLink>
