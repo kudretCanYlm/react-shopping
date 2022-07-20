c24dc6bd7e131255148dccc49418a9fb05107053
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-07-20 16:18:03 +0300

    added TextLinks and UserCard components

--- src/components/main/navbars/NavBarTop.js
+++ src/components/main/navbars/NavBarTop.js
@@ -3,34 +3,51 @@ import IconButton from '../../base/buttons/IconButton';
 import { AiOutlineMenu } from 'react-icons/ai';
 import SearchTextBox from '../../base/textboxes/SearchTextBox';
 import TextLink from '../../base/links/TextLink';
-import {  useDispatch, useSelector,connect } from 'react-redux';
-import {
-  TOGGLE_LEFT_NAV_BAR
-} from '../../../redux/actions/NavBarActions';
+import { useDispatch, useSelector, connect } from 'react-redux';
+import { TOGGLE_LEFT_NAV_BAR } from '../../../redux/actions/NavBarActions';
+import UserCard from '../cards/navBarCards/UserCard';
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   ...state.NavBarReducer
 });
 
 const NavBarTop = () => {
-
   const dispatch = useDispatch();
 
   const onClick = useCallback(() => dispatch({ type: TOGGLE_LEFT_NAV_BAR }), [dispatch]);
 
   return (
     <div className="nav-bar-top flex-row">
-      <IconButton text={'Main'} onClick={onClick}>
-        <AiOutlineMenu className={'white fontsize-dashboard'} />
-      </IconButton>
-      {/* will add to redux */}
-      <SearchTextBox textChange={() => {}} placeHolder="Search Here" className="max-width-100" />
-      <TextLink to={'/dashboard'} className={'white'}>
-        Dashboard
-      </TextLink>
-      <TextLink to={'/'} className={'white'}>
-        Test
-      </TextLink>
+
+      <div className="left flex-row flex-center-items">
+        <IconButton text={'Menu'} onClick={onClick}>
+          <AiOutlineMenu className={'white fontsize-dashboard'} />
+        </IconButton>
+        {/* will add to redux */}
+        <SearchTextBox textChange={() => { }} placeHolder="Search Here" className="max-width-100" />
+        <TextLink to={'/dashboard'} className={'white fontsize-dashboard-min link-margin-1-vertical'}>
+          Dashboard
+        </TextLink>
+        <TextLink to={'/member'} className={'white fontsize-dashboard-min link-margin-1-vertical'}>
+          Member
+        </TextLink>
+        <TextLink to={'/report'} className={'white fontsize-dashboard-min link-margin-1-vertical'}>
+          Report
+        </TextLink>
+        <TextLink to={'/help'} className={'white fontsize-dashboard-min link-margin-1-vertical'}>
+          Help
+        </TextLink>
+      </div>
+      <div className='right'>
+        <UserCard
+          user={{
+            userName: 'Kudret Can',
+            role: 'Admin',
+            imgUrl:
+              'https://image.shutterstock.com/z/stock-photo-red-apple-fruit-isolated-on-white-background-1896616390.jpg'
+          }}
+        />
+      </div>
     </div>
   );
 };
