4be570f10d1eb7308e49eedba011104b624c5d69
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-07-29 14:59:16 +0300

    added rest animations

--- src/components/base/animations/Animations.js
+++ src/components/base/animations/Animations.js
@@ -26,7 +26,53 @@ import {
   fadeOutRight,
   fadeOutRightBig,
   fadeOutUp,
-  fadeOutUpBig
+  fadeOutUpBig,
+  flip,
+  flipInX,
+  flipInY,
+  flipOutX,
+  flipOutY,
+  headShake,
+  hinge,
+  jello,
+  lightSpeedIn,
+  lightSpeedOut,
+  pulse,
+  rollIn,
+  rollOut,
+  rotateIn,
+  rotateInDownLeft,
+  rotateInDownRight,
+  rotateInUpLeft,
+  rotateInUpRight,
+  rotateOut,
+  rotateOutDownLeft,
+  rotateOutDownRight,
+  rotateOutUpLeft,
+  rotateOutUpRight,
+  rubberBand,
+  shake,
+  slideInDown,
+  slideInLeft,
+  slideInRight,
+  slideInUp,
+  slideOutDown,
+  slideOutLeft,
+  slideOutRight,
+  slideOutUp,
+  swing,
+  tada,
+  wobble,
+  zoomIn,
+  zoomInDown,
+  zoomInLeft,
+  zoomInRight,
+  zoomInUp,
+  zoomOut,
+  zoomOutDown,
+  zoomOutLeft,
+  zoomOutRight,
+  zoomOutUp
 } from 'react-animations';
 import Radium from 'radium';
 
@@ -148,4 +194,251 @@ const fades = {
   }
 };
 
-export { bounces, fades };
+const flips = {
+  flip: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(flip, 'flip')
+  },
+  flipInX: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(flipInX, 'flipInX')
+  },
+  flipInY: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(flipInY, 'flipInY')
+  },
+  flipOutX: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(flipOutX, 'flipOutX')
+  },
+  flipOutY: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(flipOutY, 'flipOutY')
+  }
+};
+
+const headShakes = {
+  headShake: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(headShake, 'headShake')
+  }
+};
+
+const hinges = {
+  hinge: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(hinge, 'hinge')
+  }
+};
+
+const jellos = {
+  jello: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(jello, 'jello')
+  }
+};
+
+const lightSpeeds = {
+  lightSpeedIn: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(lightSpeedIn, 'lightSpeedIn')
+  },
+  lightSpeedOut: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(lightSpeedOut, 'lightSpeedOut')
+  }
+};
+
+const pulses = {
+  pulse: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(pulse, 'pulse')
+  }
+};
+
+const rolls = {
+  rollIn: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(rollIn, 'rollIn')
+  },
+  rollOut: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(rollOut, 'rollOut')
+  }
+};
+
+const rotates = {
+  rotateIn: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(rotateIn, 'rotateIn')
+  },
+  rotateInDownLeft: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(rotateInDownLeft, 'rotateInDownLeft')
+  },
+  rotateInDownRight: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(rotateInDownRight, 'rotateInDownRight')
+  },
+  rotateInUpLeft: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(rotateInUpLeft, 'rotateInUpLeft')
+  },
+  rotateInUpRight: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(rotateInUpRight, 'rotateInUpRight')
+  },
+  rotateOut: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(rotateOut, 'rotateOut')
+  },
+  rotateOutDownLeft: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(rotateOutDownLeft, 'rotateOutDownLeft')
+  },
+  rotateOutDownRight: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(rotateOutDownRight, 'rotateOutDownRight')
+  },
+  rotateOutUpLeft: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(rotateOutUpLeft, 'rotateOutUpLeft')
+  },
+  rotateOutUpRight: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(rotateOutUpRight, 'rotateOutUpRight')
+  }
+};
+
+const rubberBands = {
+  rubberBand: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(rubberBand, 'rubberBand')
+  }
+};
+
+const shakes = {
+  shake: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(shake, 'shake')
+  }
+};
+
+const slides = {
+  slideInDown: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(slideInDown, 'slideInDown')
+  },
+  slideInLeft: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(slideInLeft, 'slideInLeft')
+  },
+  slideInRight: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(slideInRight, 'slideInRight')
+  },
+  slideInUp: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(slideInUp, 'slideInUp')
+  },
+  slideOutDown: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(slideOutDown, 'slideOutDown')
+  },
+  slideOutLeft: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(slideOutLeft, 'slideOutLeft')
+  },
+  slideOutRight: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(slideOutRight, 'slideOutRight')
+  },
+  slideOutUp: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(slideOutUp, 'slideOutUp')
+  }
+};
+
+const swings = {
+  swing: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(swing, 'swing')
+  }
+};
+
+const tadas = {
+  tada: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(tada, 'tada')
+  }
+};
+
+const wobbles = {
+  wobble: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(wobble, 'wobble')
+  }
+};
+
+const zooms = {
+  zoomIn: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(zoomIn, 'zoomIn')
+  },
+  zoomInDown: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(zoomInDown, 'zoomInDown')
+  },
+  zoomInLeft: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(zoomInLeft, 'zoomInLeft')
+  },
+  zoomInRight: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(zoomInRight, 'zoomInRight')
+  },
+  zoomInUp: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(zoomInUp, 'zoomInUp')
+  },
+  zoomOut: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(zoomOut, 'zoomOut')
+  },
+  zoomOutDown: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(zoomOutDown, 'zoomOutDown')
+  },
+  zoomOutLeft: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(zoomOutLeft, 'zoomOutLeft')
+  },
+  zoomOutRight: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(zoomOutRight, 'zoomOutRight')
+  },
+  zoomOutUp: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(zoomOutUp, 'zoomOutUp')
+  }
+};
+
+export {
+  bounces,
+  fades,
+  flips,
+  headShakes,
+  hinges,
+  jellos,
+  lightSpeeds,
+  pulses,
+  rolls,
+  rotates,
+  rubberBands,
+  shakes,
+  slides,
+  swings,
+  tadas,
+  wobbles,
+  zooms
+};
