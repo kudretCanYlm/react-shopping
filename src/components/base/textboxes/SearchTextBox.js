adcbe910ec93902cbc9ce1efa68d1b999791dfe7
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-08-02 17:04:23 +0300

    removed react fragment,edited react useRef hook

--- src/components/base/textboxes/SearchTextBox.js
+++ src/components/base/textboxes/SearchTextBox.js
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { BsSearch } from 'react-icons/bs';
 import { useRef } from 'react';
 import '../../../styles/common/common.css';
-import { useDispatch, useSelector, connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 const SearchTextBox = ({
   placeHolder = 'Search here..',
@@ -17,24 +17,22 @@ const SearchTextBox = ({
   console.log(isOpen);
 
   return (
-    <>
-      <div
-        className={`text-box-search flex-row ${className} ${
-          isOpen ? 'open-text-box' : 'close-text-box'
-        }`}
-      >
-        <BsSearch className="icon" />
-        <input
-          className="input-text"
-          type={'text'}
-          placeholder={placeHolder}
-          onChange={(e) => {
-            text.current = e.target.value;
-            textChange(text.current);
-          }}
-        />
-      </div>
-    </>
+    <div
+      className={`text-box-search flex-row ${className} ${
+        isOpen ? 'open-text-box' : 'close-text-box'
+      }`}
+    >
+      <BsSearch className="icon" />
+      <input
+        className="input-text"
+        type={'text'}
+        placeholder={placeHolder}
+        onChange={() => {
+          textChange(text.current.value);
+        }}
+        ref={text}
+      />
+    </div>
   );
 };
 
