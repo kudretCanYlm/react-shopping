dc5c4a7e813d3334c841220a2fe8a2962e41defd
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-08-06 17:19:45 +0300

    default props added

--- src/components/base/texts/TextA.js
+++ src/components/base/texts/TextA.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const TextA = ({ children, color = 'purple' }) => {
+const TextA = ({ children, color }) => {
   return <p className={'text-a ' + color}>{children}</p>;
 };
 
-export default TextA;
-
+//proptypes
 TextA.propTypes = {
   children: PropTypes.string.isRequired,
   color: PropTypes.string
 };
+
+//default props
+TextA.defaultProps = {
+  color: 'purple'
+};
+
+export default TextA;
