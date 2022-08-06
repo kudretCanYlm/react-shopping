402783e40727d75076e523fbec2f76464c3367be
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-08-06 17:11:55 +0300

    first commit

---
+++ src/components/base/listboxes/ListBoxTypeA.js
@@ -0,0 +1,51 @@
+import React, { useRef } from 'react';
+import PropTypes from 'prop-types';
+
+const ListBoxTypeA = ({ list, name, onChangeSelect = (f) => f, className }) => {
+  let selected = useRef();
+
+  const takeSelected = () => {
+    onChangeSelect(selected.current.value);
+  };
+
+  return (
+    <select
+      ref={selected}
+      onChange={takeSelected}
+      name={name}
+      className={`list-box list-box-type-a ${className}`}
+    >
+      {list.map((option, key) => (
+        <option key={key} value={option.value}>
+          {option.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+//proptypes
+ListBoxTypeA.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.any.isRequired,
+      value: PropTypes.any.isRequired
+    })
+  ),
+  name: PropTypes.string.isRequired,
+  className: PropTypes.string
+};
+
+//default props
+ListBoxTypeA.defaultProps = {
+  list: [
+    {
+      name: 'empty',
+      value: 'empty'
+    }
+  ],
+  name: 'not named',
+  className: ''
+};
+
+export default ListBoxTypeA;
