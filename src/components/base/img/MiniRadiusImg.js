1aa993901413f27d2ea5fa10d3f20af705e25a84
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-08-06 17:07:09 +0300

    default props and className prop added

--- src/components/base/img/MiniRadiusImg.js
+++ src/components/base/img/MiniRadiusImg.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { DEFAULT_IMG_URL } from './common/common-props';
 
-const MiniRadiusImg = ({ url }) => {
+const MiniRadiusImg = ({ url, className }) => {
   return (
-    <div className="img mini-img">
+    <div className={`img mini-img ${className}`}>
       <img alt="" src={url} />
     </div>
   );
@@ -11,7 +12,14 @@ const MiniRadiusImg = ({ url }) => {
 
 //prop-type
 MiniRadiusImg.propTypes = {
-  url: PropTypes.string.isRequired
+  url: PropTypes.string.isRequired,
+  className: PropTypes.string
+};
+
+//default props
+MiniRadiusImg.defaultProps = {
+  url: DEFAULT_IMG_URL,
+  className: ''
 };
 
 export default MiniRadiusImg;
