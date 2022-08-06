b86088039f420a15c1f9440a7eedb45df109e35f
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-08-06 17:02:43 +0300

    default props added

--- src/components/base/icons/ArrowUpDownIcon.js
+++ src/components/base/icons/ArrowUpDownIcon.js
@@ -2,16 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { BsFillArrowUpCircleFill } from 'react-icons/bs';
 
-const ArrowUpDownIcon = ({ fontSize = 20, className = '' }) => {
+const ArrowUpDownIcon = ({ fontSize, className }) => {
   return (
     <BsFillArrowUpCircleFill fontSize={fontSize} className={`arrow-up-down-icon ${className}`} />
   );
 };
-
-export default ArrowUpDownIcon;
 
 //prototypes
 ArrowUpDownIcon.propTypes = {
   fontSize: PropTypes.number.isRequired,
   className: PropTypes.string
 };
+
+//default props
+ArrowUpDownIcon.defaultProps = {
+  fontSize: 20,
+  className: ''
+};
+
+export default ArrowUpDownIcon;
