782ea508d47caa967b47e0da52fc8dbbad5501a2
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-08-06 17:10:00 +0300

    default props and prop types added

--- src/components/base/links/IconLink.js
+++ src/components/base/links/IconLink.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import '../../../styles/base/base.css';
+import { EMPTY_ICON } from './common/common-props';
 
-const IconLink = ({ children, text, to = '/' }) => {
+const IconLink = ({ children, text, to }) => {
   return (
     <div className="link-base">
       <Link className="link link-icon flex-row" to={to}>
@@ -11,6 +12,20 @@ const IconLink = ({ children, text, to = '/' }) => {
       </Link>
     </div>
   );
+};
+
+//proptypes
+IconLink.propTypes = {
+  children: PropTypes.object.isRequired,
+  text: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired
+};
+
+//default props
+IconLink.defaultProps = {
+  children: EMPTY_ICON,
+  text: 'Empty',
+  to: '/'
 };
 
 export default IconLink;
