7e680f0440e5160686fbf9f8d5ec36ad3ae44e0b
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-08-07 20:41:32 +0300

    first commit

---
+++ src/components/base/charts/common/common-props.js
@@ -0,0 +1,4 @@
+const DEFAULT_X_VALUES = ['Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty'];
+const DEFAULT_Y_VALUES = [5, 10, 15, 20, 25, 30, 35, 40];
+
+export { DEFAULT_X_VALUES, DEFAULT_Y_VALUES };
