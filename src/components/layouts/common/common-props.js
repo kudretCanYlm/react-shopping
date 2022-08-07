808acb3f6f6687f0ddbc790c73bc0ca1bd50a0db
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-08-07 20:46:30 +0300

    first commit

---
+++ src/components/layouts/common/common-props.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TextA from 'components/base/texts/TextA';
+
+const NO_CONTENT_ERROR = () => {
+  return (
+    <div className="flex-row center-text">
+      <TextA>No Content Here</TextA>
+    </div>
+  );
+};
+
+export { NO_CONTENT_ERROR };
