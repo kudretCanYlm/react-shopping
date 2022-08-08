f6e9096b51a9ad736ae33c2a121b8b946c016789
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-08-08 16:23:46 +0300

    added default props

--- src/components/main/cards/SalesCardInfo.js
+++ src/components/main/cards/SalesCardInfo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const SalesCardInfo = ({ title, info, className = '' }) => {
+const SalesCardInfo = ({ title, info, className }) => {
   return (
     <div className={`sales-card-info ${className}`}>
       <h1>{title}</h1>
@@ -10,11 +10,18 @@ const SalesCardInfo = ({ title, info, className = '' }) => {
   );
 };
 
-export default SalesCardInfo;
-
 //propTypes
 SalesCardInfo.propTypes = {
   title: PropTypes.string.isRequired,
   info: PropTypes.string.isRequired,
   className: PropTypes.string
 };
+
+//default props
+SalesCardInfo.defaultProps = {
+  title: 'Empty',
+  info: 'Empty',
+  className: ''
+};
+
+export default SalesCardInfo;
