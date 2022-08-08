29792f64f2a9b1d8cc0cec59294063049adce769
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-08-08 16:38:55 +0300

    eslint fix

--- src/redux/stores/Store.js
+++ src/redux/stores/Store.js
@@ -3,7 +3,7 @@ import { createLogger } from 'redux-logger';
 import { createDevTools } from '@redux-devtools/core';
 import Reducers from '../reducers/Reducer';
 
-//devam et https://github.com/gothinkster/react-redux-realworld-example-app/blob/master/src/store.js
+//look https://github.com/gothinkster/react-redux-realworld-example-app/blob/master/src/store.js
 
 const Store = createStore(Reducers);
 
