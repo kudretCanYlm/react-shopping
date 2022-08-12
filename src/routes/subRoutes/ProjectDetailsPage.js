af4b440238e6f08b1c1345c761c572f83b3d9037
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-08-12 20:56:04 +0300

    slider component added

--- src/routes/subRoutes/ProjectDetailsPage.js
+++ src/routes/subRoutes/ProjectDetailsPage.js
@@ -1,10 +1,15 @@
+import DetailsSlider from 'components/base/sliders/DetailsSlider';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
 const ProjectDetailsPage = () => {
   let { projectId } = useParams();
 
-  return <>project details page {projectId}</>;
+  return (
+    <>
+      <DetailsSlider />
+    </>
+  );
 };
 
 export default ProjectDetailsPage;
