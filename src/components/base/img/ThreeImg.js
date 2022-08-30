a22de334a9e02eb759c6a1c2970b37be29cc05f1
Kudret Can <kudretcanylm@gmail.com>
2022-08-30 11:49:02 +0300

    added prop-types and default props

--- src/components/base/img/ThreeImg.js
+++ src/components/base/img/ThreeImg.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { IMG_1, IMG_2, IMG_3 } from './common/stock-images';
 
 const ThreeImg = ({ img_1, img_2, img_3, className }) => {
   return (
@@ -8,6 +10,22 @@ const ThreeImg = ({ img_1, img_2, img_3, className }) => {
       <img className="transform-2" src={img_3} />
     </div>
   );
+};
+
+//prop-types
+ThreeImg.propTypes = {
+  img_1: PropTypes.string.isRequired,
+  img_2: PropTypes.string.isRequired,
+  img_3: PropTypes.string.isRequired,
+  className: PropTypes.string
+};
+
+//default props
+ThreeImg.defaultProps = {
+  img_1: IMG_1,
+  img_2: IMG_2,
+  img_3: IMG_3,
+  className: ''
 };
 
 export default ThreeImg;
