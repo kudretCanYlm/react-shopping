ad27a694765a6e74e2d48feb652a6ddefb8ee5b1
Kudret Can <kudretcanylm@gmail.com>
2022-09-02 11:53:41 +0300

    added icons and text

--- src/components/main/footers/DetailsFooter.js
+++ src/components/main/footers/DetailsFooter.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import IconLink from 'components/base/links/IconLink';
+import { BsLinkedin, BsGithub, BsInstagram } from 'react-icons/bs';
+import BorderIcon from 'components/base/icons/BorderIcon';
+import { Link } from 'react-router-dom';
 
 const DetailsFooter = () => {
-  return <footer className="details-footer"></footer>;
+  const iconFontSize = 25;
+
+  return (
+    <footer className="details-footer flex-column">
+      <div className="footer-icons flex-row">
+        <BorderIcon>
+          <BsLinkedin fontSize={iconFontSize} className="icon-color-2" />
+        </BorderIcon>
+        <BorderIcon>
+          <BsGithub fontSize={iconFontSize} className="icon-color-2" />
+        </BorderIcon>
+        <BorderIcon>
+          <BsInstagram fontSize={iconFontSize} className="icon-color-2" />
+        </BorderIcon>
+      </div>
+      <p>&copy; 2022 Kudret Can Yalım</p>
+    </footer>
+  );
 };
 
 export default DetailsFooter;
