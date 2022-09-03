cf2b9f217ee81b8a7d50119729ea75d4f91e2466
Kudret Can <kudretcanylm@gmail.com>
2022-09-03 16:56:20 +0300

    applied useSearchParams hook and added isWithParams prop

--- src/components/base/textboxes/SearchTextBox.js
+++ src/components/base/textboxes/SearchTextBox.js
@@ -2,27 +2,38 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { BsSearch } from 'react-icons/bs';
 import { useRef } from 'react';
-import '../../../styles/common/common.css';
+import { useSearchParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
-const SearchTextBox = ({ placeHolder, textChange = (t) => t, className }) => {
+const SearchTextBox = ({ placeHolder, isWithParams, textChange = (t) => t, className }) => {
   let text = useRef();
 
   let isOpen = useSelector((state) => state.TextBoxReducer).isOpen;
+  const [searchParams, setSearchParams] = useSearchParams();
 
-  console.log(isOpen);
+  if (isWithParams) {
+    useEffect(() => {
+      text.current.value = searchParams.get('src') ?? '';
+    }, []);
+  }
 
   return (
     <div
       className={`text-box-search flex-row ${className} ${
         isOpen ? 'open-text-box' : 'close-text-box'
-      }`}>
+      }`}
+    >
       <BsSearch className="icon" />
       <input
         className="input-text"
         type={'text'}
         placeholder={placeHolder}
         onChange={() => {
+          if (isWithParams) {
+            if (text.current.value.length == 0) setSearchParams();
+            else setSearchParams({ ['src']: text.current.value });
+          }
           textChange(text.current.value);
         }}
         ref={text}
@@ -35,12 +46,14 @@ const SearchTextBox = ({ placeHolder, textChange = (t) => t, className }) => {
 SearchTextBox.propTypes = {
   placeHolder: PropTypes.string,
   textChange: PropTypes.func.isRequired,
+  isWithParams: PropTypes.bool.isRequired,
   className: PropTypes.string
 };
 
 //default props
 SearchTextBox.defaultProps = {
   placeHolder: 'Search here..',
+  isWithParams: false,
   className: ''
 };
 
