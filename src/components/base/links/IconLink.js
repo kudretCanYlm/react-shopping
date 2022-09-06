ce8cdc12bfc235dbc83db65b986056d5ee1b3b2c
Kudret Can <kudretcanylm@gmail.com>
2022-09-06 12:01:35 +0300

    added className prop

--- src/components/base/links/IconLink.js
+++ src/components/base/links/IconLink.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { EMPTY_ICON } from './common/common-props';
 
-const IconLink = ({ children, text, to }) => {
+const IconLink = ({ children, text, to, className }) => {
   return (
-    <div className="link-base">
+    <div className={`link-base ${className}`}>
       <Link className="link link-icon flex-row" to={to}>
         <div className="icon">{children}</div>
         <h2>{text}</h2>
@@ -18,14 +18,16 @@ const IconLink = ({ children, text, to }) => {
 IconLink.propTypes = {
   children: PropTypes.object.isRequired,
   text: PropTypes.string.isRequired,
-  to: PropTypes.string.isRequired
+  to: PropTypes.string.isRequired,
+  className: PropTypes.string
 };
 
 //default props
 IconLink.defaultProps = {
   children: EMPTY_ICON,
   text: 'Empty',
-  to: '/'
+  to: '/',
+  className: ''
 };
 
 export default IconLink;
