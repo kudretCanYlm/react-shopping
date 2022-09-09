8b8e75d883444f22141c6c562d195a260bd416f1
Kudret Can <kudretcanylm@gmail.com>
2022-09-09 12:25:10 +0300

    eslint-fix

--- src/components/base/sliders/VerticalSlider.js
+++ src/components/base/sliders/VerticalSlider.js
@@ -28,7 +28,8 @@ const VerticalSlider = ({ images, className }) => {
         loop
         mousewheel={true}
         scrollbar={{ draggable: true }}
-        className="slider">
+        className="slider"
+      >
         {images.map((image, key) => (
           <SwiperSlide key={key} className="flex-column slide">
             <img src={image} />
