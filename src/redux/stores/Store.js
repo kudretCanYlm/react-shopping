f9e5ecd6583559ba3a24740b62f2ca9e61c76a2b
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-09-23 18:23:30 +0300

    added logger middleware

--- src/redux/stores/Store.js
+++ src/redux/stores/Store.js
@@ -2,9 +2,25 @@ import { applyMiddleware, createStore } from 'redux';
 import { createLogger } from 'redux-logger';
 import { createDevTools } from '@redux-devtools/core';
 import Reducers from '../reducers/Reducer';
-
+//https://github.com/AliYildizoz909/photo-channel-spa/tree/master/src/redux/actions
 //look https://github.com/gothinkster/react-redux-realworld-example-app/blob/master/src/store.js
 
-const Store = createStore(Reducers);
+function logger({ getState }) {
+  return (next) => (action) => {
+    console.log('test');
+    console.log('will dispatch', action);
+
+    // Call the next dispatch method in the middleware chain.
+    const returnValue = next(action);
+
+    console.log('state after dispatch', getState());
+
+    // This will likely be the action itself, unless
+    // a middleware further in chain changed it.
+    return returnValue;
+  };
+}
+
+const Store = createStore(Reducers, applyMiddleware(logger));
 
 export default Store;
