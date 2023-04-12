0ed7cc17d7abbccd3d07a807bd69b9625d8a9d88
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-04-12 17:18:23 +0300

    changed as class ,set token option,set content type

--- src/api/utils/base-api.js
+++ src/api/utils/base-api.js
@@ -1,13 +1,19 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://localhost:44369';
-const TIME_OUT = 10000;
-const TOKEN =
-  'OAEvE7VUdqIkH+vO2/NqsWLjSrMiUcBKJsSR/9UBK2+Es515Y3iGmNJigw3MrRXB5ZNNbVWeDr6+SQIaSAc5dNLYMhGOD9oXNv0eofedQJ/oHnvEn/XzoGppk6KmibfDmgUiniqp2Pa/YFr2abduiVDT0GElddlJNUAlYv/6Sh03kFNCksjSpmUl1UbE7Rig63pNqDtiSlYTCp35sjXFjl1fbBrb2RyrK74+nmkeblTR3D8C4f9SOePmd9ZZLNVZWvCyhWmfTSNhyaipfHb6j++OZ/K8x4HcFBmf6PAgIaq6JgIAax2pz0oPfd6k9Qpd';
-const BaseApi = axios.create({
-  baseURL: BASE_URL,
-  timeout: TIME_OUT,
-  headers: { Authorization: 'Bearer ' + TOKEN }
-});
+class BaseApiClass {
+  constructor() {
+    this.token = JSON.parse(localStorage.getItem('token'));
+    this.baseURL = 'https://localhost:44369';
+    this.timeOut = 10000;
+  }
 
-export default BaseApi;
+  BaseApi() {
+    return axios.create({
+      baseURL: this.baseURL,
+      timeout: this.timeOut,
+      headers: { Authorization: 'Bearer ' + this.token, 'Content-Type': 'application/json' }
+    });
+  }
+}
+
+export default BaseApiClass;
