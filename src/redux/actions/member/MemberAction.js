bf40fde37f368f0d52b08a0cede94438a64f19bd
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-04-12 17:25:46 +0300

    refactored

--- src/redux/actions/member/MemberAction.js
+++ src/redux/actions/member/MemberAction.js
@@ -1,28 +1,33 @@
 import { GetMembersByRange } from 'api/member/member-api';
+import {
+  GetUserRoleAndDescriptionByPagedList,
+  GetUserRoleAndDescriptionByRange
+} from 'api/user/user-api';
 
 //types
 const MEMBER_IS_LOADING = 'MEMBER_IS_LOADING';
 const MEMBER_LOADING_ERROR = 'MEMBER_LOADING_ERROR';
 const MEMBER_LOADED = 'MEMBER_LOADED';
-const MEMBER_CLEAR = "MEMBER_CLEAR";
-const MEMBER_SET_PAGE = "MEMBER_SET_PAGE";
-const MEMBER_SET_END = "MEMBER_SET_END";
-
+const MEMBER_CLEAR = 'MEMBER_CLEAR';
+const MEMBER_SET_PAGE = 'MEMBER_SET_PAGE';
+const MEMBER_SET_END = 'MEMBER_SET_END';
 
 //types functions
 const memberIsLoading = (bool) => {
   return {
     type: MEMBER_IS_LOADING,
-    isLoading: bool
+    isLoading: bool,
+    isEnd: false
   };
 };
 
 const memberLoadingError = (err) => {
   return {
     type: MEMBER_LOADING_ERROR,
-    errCode: err.code,
+    errCode: err.response.status,
     errMessage: err.message,
-    isError: true
+    isError: true,
+    isEnd: false
   };
 };
 
@@ -31,7 +36,8 @@ const memberLoaded = (data, isEnd) => {
     type: MEMBER_LOADED,
     payload: data,
     loaded: true,
-    isEnd
+    isEnd,
+    isError: false
   };
 };
 
@@ -50,29 +56,34 @@ const memberSetPage = () => {
 //actions
 
 //get
-const GET_MEMBERS_BY_RANGE = (pageNumber, pageSize) => {
-  return (dispatch) => {
-    dispatch(memberIsLoading(true));
+const GET_MEMBERS_BY_RANGE = (pageNumber, pageSize, orderBys = [], filters = []) => {
+  return (dispatch, getState) => {
+    if (getState().MemberReducer.isEnd == false) {
+      dispatch(memberIsLoading(true));
 
-    GetMembersByRange(pageNumber, pageSize)
-      .then((response) => {
-        dispatch(memberIsLoading(false));
+      GetUserRoleAndDescriptionByPagedList(pageNumber, pageSize, orderBys, filters)
+        .then((response) => {
+          dispatch(memberIsLoading(false));
 
-        if ([...response.data].length == 0)
-          dispatch(memberLoaded(response.data, true));
-        else {
-          dispatch(memberLoaded(response.data, false));
-          dispatch(memberSetPage());
-        }
-
-      })
-      .catch((err) => {
-        dispatch(memberIsLoading(false));
-        dispatch(memberLoadingError(err));
-      });
+          if ([...response.data].length == 0) dispatch(memberLoaded(response.data, true));
+          else {
+            dispatch(memberLoaded(response.data, false));
+            dispatch(memberSetPage());
+          }
+        })
+        .catch((err) => {
+          dispatch(memberIsLoading(false));
+          dispatch(memberLoadingError(err));
+        });
+    }
   };
 };
 
+const SET_MEMBER_CLEAR = () => {
+  return (dispatch) => {
+    dispatch(memberClear());
+  };
+};
 //posts
 
 export {
@@ -80,7 +91,8 @@ export {
   MEMBER_LOADED,
   MEMBER_LOADING_ERROR,
   GET_MEMBERS_BY_RANGE,
+  SET_MEMBER_CLEAR,
   MEMBER_CLEAR,
   MEMBER_SET_PAGE,
-  MEMBER_SET_END,
+  MEMBER_SET_END
 };
