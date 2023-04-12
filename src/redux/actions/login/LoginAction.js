700656c09889afd5c74a8252a447fea3555609db
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-04-12 17:25:30 +0300

    remaked

--- src/redux/actions/login/LoginAction.js
+++ src/redux/actions/login/LoginAction.js
@@ -1,37 +1,53 @@
 import { Login } from 'api/login/login-api';
 
 //types
-const USER_LOGIN_INFO = 'USER_LOGIN_INFO';
-const USER_LOGIN_STATE = 'USER_LOGIN_STATE';
+const USER_IS_LOGINING = 'USER_IS_LOGINING';
+const USER_LOGGED = 'USER_LOGGED';
+const USER_LOGIN_ERROR = 'USER_LOGIN_ERROR';
+const USER_LOGOUT = 'USER_LOGOUT';
+const GET_USER_TOKEN = 'GET_USER_TOKEN';
 
 //types functions
-const userLoginBool = (isLogin) => {
+
+const userIsLogining = (isLogining) => {
   return {
-    type: USER_LOGIN_STATE,
-    isLogin
+    type: USER_IS_LOGINING,
+    isLogining
   };
 };
 
-const userLogin = (token) => {
+const userLoginError = (err) => {
   return {
-    type: USER_LOGIN_INFO,
-    token
+    type: USER_LOGIN_ERROR,
+    errCode: err.response.status,
+    errMessage: err.response.data,
+    isError: true
+  };
+};
+
+const userLogged = (isLogged) => {
+  return {
+    type: USER_LOGGED,
+    isLogged
+  };
+};
+
+const userLogout = (isLogged) => {
+  return {
+    type: USER_LOGOUT,
+    isLogged
   };
 };
 
 //actions
 
 //get
-//add as midllee ware
 const GET_TOKEN_FROM_LOCAL = () => {
   return (dispatch) => {
     const token = JSON.parse(localStorage.getItem('token'));
 
-    if (token.length < 5) {
-      dispatch(userLoginBool(false));
+    if (token) {
     } else {
-      dispatch(userLogin(token));
-      dispatch(userLoginBool(true));
     }
   };
 };
@@ -39,19 +55,27 @@ const GET_TOKEN_FROM_LOCAL = () => {
 //post
 const POST_LOGIN = (login) => {
   return (dispatch) => {
-    dispatch(userLoginBool(false));
-
+    dispatch(userIsLogining(true));
+    console.log('gridi2');
     Login(login)
       .then((response) => {
-        localStorage.setItem('token', JSON.stringify(response.data));
-        dispatch(userLogin(response.data));
-        dispatch(userLoginBool(true));
+        setItem('token', JSON.stringify(response.data)).then(() => {
+          dispatch(userLogged(true));
+          dispatch(userIsLogining(false));
+        });
       })
       .catch((err) => {
-        localStorage.removeItem('token');
-        dispatch(userLoginBool(false));
+        console.log(err);
+        dispatch(userIsLogining(false));
+        dispatch(userLoginError(err));
       });
   };
 };
 
-export { USER_LOGIN_INFO, USER_LOGIN_STATE, GET_TOKEN_FROM_LOCAL, POST_LOGIN };
+export { USER_IS_LOGINING, USER_LOGGED, USER_LOGIN_ERROR, USER_LOGOUT, POST_LOGIN };
+
+function setItem(key, value) {
+  return Promise.resolve().then(function () {
+    localStorage.setItem(key, value);
+  });
+}
