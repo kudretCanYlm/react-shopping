c62e9cd61cdf1344f01e0b26da0047bcc2bbc041
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-05-21 15:53:05 +0300

    Added SIGNUP Actions

--- src/redux/actions/login/LoginAction.js
+++ src/redux/actions/login/LoginAction.js
@@ -1,14 +1,20 @@
-import { Login } from 'api/login/login-api';
+import { Login, SignUp } from 'api/login/login-api';
 
 //types
+//login
 const USER_IS_LOGINING = 'USER_IS_LOGINING';
 const USER_LOGGED = 'USER_LOGGED';
 const USER_LOGIN_ERROR = 'USER_LOGIN_ERROR';
 const USER_LOGOUT = 'USER_LOGOUT';
+
+//signup
 const GET_USER_TOKEN = 'GET_USER_TOKEN';
+const USER_IS_SIGNUPPING = 'USER_IS_SIGNUPPING';
+const USER_SIGNUPPED = 'USER_SIGNUPPED';
+const USER_SIGNUP_ERROR = 'USER_SIGNUP_ERROR';
 
 //types functions
-
+//login
 const userIsLogining = (isLogining) => {
   return {
     type: USER_IS_LOGINING,
@@ -39,8 +45,33 @@ const userLogout = (isLogged) => {
   };
 };
 
-//actions
+//signup
 
+const userIsSignUpping = (isSignUpping) => {
+  return {
+    type: USER_IS_SIGNUPPING,
+    isSignUpping
+  };
+};
+
+const userSignUpError = (err) => {
+  return {
+    type: USER_SIGNUP_ERROR,
+    errCode: err.response.status,
+    errMessage: err.response.data,
+    isError: true
+  };
+};
+
+const userSignUpped = (isSignUpped) => {
+  return {
+    type: USER_SIGNUPPED,
+    isSignUpped
+  };
+};
+
+//actions
+//login
 //get
 const GET_TOKEN_FROM_LOCAL = () => {
   return (dispatch) => {
@@ -56,7 +87,7 @@ const GET_TOKEN_FROM_LOCAL = () => {
 const POST_LOGIN = (login) => {
   return (dispatch) => {
     dispatch(userIsLogining(true));
-    console.log('gridi2');
+
     Login(login)
       .then((response) => {
         setItem('token', JSON.stringify(response.data)).then(() => {
@@ -65,14 +96,44 @@ const POST_LOGIN = (login) => {
         });
       })
       .catch((err) => {
-        console.log(err);
         dispatch(userIsLogining(false));
         dispatch(userLoginError(err));
       });
   };
 };
 
-export { USER_IS_LOGINING, USER_LOGGED, USER_LOGIN_ERROR, USER_LOGOUT, POST_LOGIN };
+//post
+const POST_SIGNUP = (signup) => {
+  return (dispatch) => {
+    dispatch(userIsSignUpping(true));
+
+    console.log(signup);
+
+    SignUp(signup)
+      .then((response) => {
+        setItem('token', JSON.stringify(response.data)).then(() => {
+          dispatch(userSignUpped(true));
+          dispatch(userIsSignUpping(false));
+        });
+      })
+      .catch((err) => {
+        dispatch(userIsSignUpping(false));
+        dispatch(userSignUpError(err));
+      });
+  };
+};
+
+export {
+  USER_IS_LOGINING,
+  USER_LOGGED,
+  USER_LOGIN_ERROR,
+  USER_LOGOUT,
+  USER_IS_SIGNUPPING,
+  USER_SIGNUPPED,
+  USER_SIGNUP_ERROR,
+  POST_LOGIN,
+  POST_SIGNUP
+};
 
 function setItem(key, value) {
   return Promise.resolve().then(function () {
