7c9a0b4d7e34ffa8f40efcf356df8d7188ad3a15
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-05-21 15:53:58 +0300

    Added SIGNUP Reducer

--- src/redux/reducers/login/LoginReducer.js
+++ src/redux/reducers/login/LoginReducer.js
@@ -1,14 +1,24 @@
 import {
   USER_IS_LOGINING,
+  USER_IS_SIGNUPPING,
   USER_LOGGED,
   USER_LOGIN_ERROR,
-  USER_LOGOUT
+  USER_LOGOUT,
+  USER_SIGNUPPED,
+  USER_SIGNUP_ERROR
 } from 'redux/actions/login/LoginAction';
 
-const LoginReducer = (
-  state = { isLogining: false, isLogged: false, errCode: null, errMessage: null, isError: false },
-  action
-) => {
+const defaultState = {
+  isLogining: false,
+  isLogged: false,
+  errCode: null,
+  errMessage: null,
+  isError: false,
+  isSignUpping: false,
+  isSignUpped: false
+};
+
+const LoginReducer = (state = defaultState, action) => {
   switch (action.type) {
     case USER_IS_LOGINING:
       return {
@@ -33,6 +43,26 @@ const LoginReducer = (
     case USER_LOGOUT:
       return {
         ...state
+      };
+
+    case USER_IS_SIGNUPPING:
+      return {
+        ...state,
+        isSignUpping: action.isSignUpping
+      };
+
+    case USER_SIGNUPPED:
+      return {
+        ...state,
+        isSignUpped: action.isSignUpped
+      };
+
+    case USER_SIGNUP_ERROR:
+      return {
+        ...state,
+        errCode: action.errCode,
+        errMessage: action.errMessage,
+        isError: action.isError
       };
 
     default:
